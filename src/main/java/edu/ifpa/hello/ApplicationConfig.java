9409f0985f361fcc3fc846b72fbf00fe4de3f70e
cmartins-ifpa <claudio.martins@ifpa.edu.br>
2022-03-22 17:07:12 -0300

    first commit

---
+++ src/main/java/edu/ifpa/hello/ApplicationConfig.java
@@ -0,0 +1,15 @@
+package edu.ifpa.hello;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+/**
+ * Configura a aplicação Jakarta (exemplo com REST)
+ * 
+ * @author Claudio Martins
+ */
+@ApplicationPath("")
+public class ApplicationConfig extends Application {
+    
+}
---
+++ src/main/java/edu/ifpa/hello/Hello.java
@@ -0,0 +1,23 @@
+package edu.ifpa.hello;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Um endpoint simples para "Hello World".
+ * 
+ * @author Claudio Martins
+ */
+@Path("hello")
+public class Hello {
+    
+    @GET
+    @Produces("text/plain;charset=utf-8")  
+    public Response hello(){
+        return Response.ok("Alô, bem-vindo à plataforma Jakarta EE!").build();
+    }
+        
+}
